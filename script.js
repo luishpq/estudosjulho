77cdd61d674cdc8e70574a1e0c65af6dc0c199ed
Luis <luishpassos@Hotmail.com>
2018-08-02 23:27:48 -0300

    Site com modal

---
+++ script.js
@@ -0,0 +1,8 @@
+let botao = document.querySelector(".botao1");
+botao.onclick = abreModal;
+modal = document.querySelector(".modal");
+
+function abreModal (){
+    modal.style.display = "flex";
+    // h1.style.color = "yellow";
+}
